cfb46cf750fd0aa0aff1d69b899a1b35c2019ba4
paulohspdev-cmyk <paulohspdev@gmail.com>
2026-08-28 10:43:40 -0300

    feat: adiciona leitura de tendências do Rapid SCADA

--- rapid/reader/Program.cs
+++ rapid/reader/Program.cs
@@ -1,17 +1,18 @@
+using System.Globalization;
 using System.Text.Json;
 using System.Xml;
 using Scada.Client;
+using Scada.Data.Models;
 
 if (args.Length < 2)
 {
-    Console.Error.WriteLine("Uso: RcRapidReader <ScadaCommConfig.xml> <canal> [canal...]");
+    Console.Error.WriteLine("Uso: RcRapidReader <ScadaCommConfig.xml> <canal> [canal...] | current <canal...> | trend <archiveBit> <canal> <inicio-ISO> <fim-ISO>");
     return 2;
 }
 
 try
 {
     string configFile = args[0];
-    int[] cnlNums = args.Skip(1).Select(int.Parse).ToArray();
 
     XmlDocument xmlDoc = new XmlDocument();
     xmlDoc.Load(configFile);
@@ -20,8 +21,52 @@ try
 
     ConnectionOptions options = new ConnectionOptions();
     options.LoadFromXml(connNode);
-
     ScadaClient client = new ScadaClient(options);
+
+    if (string.Equals(args[1], "trend", StringComparison.OrdinalIgnoreCase))
+    {
+        if (args.Length != 6)
+        {
+            Console.Error.WriteLine("Uso: RcRapidReader <config> trend <archiveBit> <canal> <inicio-ISO> <fim-ISO>");
+            return 2;
+        }
+
+        int archiveBit = int.Parse(args[2], CultureInfo.InvariantCulture);
+        int cnlNum = int.Parse(args[3], CultureInfo.InvariantCulture);
+        DateTime start = DateTime.Parse(args[4], CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind).ToUniversalTime();
+        DateTime end = DateTime.Parse(args[5], CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind).ToUniversalTime();
+        if (end <= start)
+            throw new Exception("Intervalo de tendência inválido");
+
+        Trend trend = client.GetTrend(archiveBit, new TimeRange(start, end, true), cnlNum);
+        var points = trend.Points.Select(p => new
+        {
+            timestamp = p.Timestamp.ToUniversalTime().ToString("O"),
+            val = p.Val,
+            stat = p.Stat,
+            defined = p.Stat > 0
+        }).ToArray();
+
+        Console.WriteLine(JsonSerializer.Serialize(new
+        {
+            ok = true,
+            archive_bit = archiveBit,
+            cnl = cnlNum,
+            start = start.ToString("O"),
+            end = end.ToString("O"),
+            points
+        }));
+        return 0;
+    }
+
+    int firstChannelArg = string.Equals(args[1], "current", StringComparison.OrdinalIgnoreCase) ? 2 : 1;
+    if (args.Length <= firstChannelArg)
+    {
+        Console.Error.WriteLine("Informe pelo menos um canal");
+        return 2;
+    }
+
+    int[] cnlNums = args.Skip(firstChannelArg).Select(int.Parse).ToArray();
     var data = client.GetCurrentData(cnlNums, false, out long listId);
 
     var channels = cnlNums.Select((cnl, i) => new
