490ce2abc38461a3f04d30025fcc88a7bbd00df1
paulohspdev-cmyk <paulohspdev@gmail.com>
2026-08-30 08:47:08 -0300

    refactor(assets): remove shim legado de imagens

--- src/data/controller-images.ts
+++
@@ -1,3 +0,0 @@
-// Compatibilidade temporária: assets visuais pertencem a src/assets.
-// Novos imports devem usar @/assets/controllers.
-export { CONTROLLER_IMAGE_FALLBACK, controllerImageSrc } from "@/assets/controllers";
